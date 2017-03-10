f2905c4723ab471a2fb518fdfe6235e2b46f5587
radityagumay <gumay.raditya@yahoo.com>
2017-03-10 14:35:36 +0700

    update code

--- com/radityalabs/benchmark/sql_insert.py
+++ com/radityalabs/benchmark/sql_insert.py
@@ -1,7 +1,8 @@
 import mysql.connector
 import csv
-from HTMLParser import HTMLParser
 import requests
+from HTMLParser import HTMLParser
+from BeautifulSoup import BeautifulSoup
 
 csv.field_size_limit(500 * 1024 * 1024)
 
@@ -17,43 +18,47 @@ url = 'http://text-processing.com/demo/sentiment/'
 
 class Parser(HTMLParser):
     def __init__(self, review_id, review_name, review_body):
+        self.review_id = review_id
+        self.review_name = reviewName
+        self.review_body = review_body
         print "id", review_id
         print "reviewName", review_name
         print "reviewBody", review_body
 
-        HTMLParser.__init__(self)
+        # HTMLParser.__init__(self)
+        # self.feed(self.request.text)
         payload = {'language': 'english', 'text': review_body}
         self.request = requests.post(url, data=payload)
-        self.feed(self.request.text)
+        self.html = BeautifulSoup(self.request.text)
+        # print parsed_html.body.find('div', attrs={'class': 'container'}).text
+        self.sentiment_collection = self.html.body.find('div', attrs={'class': 'span-9 last'})
+        #self.html_sentiment_collection = BeautifulSoup(self.sentiment_collection)
+        print self.sentiment_collection
+
+
+        # print self.html
 
     def handle_data(self, data):
         if "pos:" in data:
             positive = data.split(None)
-            print positive[1]
-        elif "pos" in data:
-            print "sentiment is positive"
-
+            val_pos = positive[1]
+            insert(self.review_id, self.reviewName, self.review_body, val_pos, 0, 0, 0)
+            print "positive", positive[1]
         if "neg:" in data:
             negative = data.split(None)
-            print negative[1]
-        elif "neg" in data:
-            print "sentiment is negative"
-
+            print "negative", negative[1]
         if "neutral:" in data:
             neutral = data.split(None)
-            print neutral[1]
-        elif "neutral" in data:
-            print "sentiment is neutral"
-
+            print "neutral", neutral[1]
         if "polar:" in data:
             polarity = data.split(None)
-            print polarity[1]
+            print "polarity", polarity[1]
 
 
-# def insert(document):
-#    query = "INSERT INTO " + table_name + " VALUES (" + query_plain + ")"
-#    cursor.execute(query)
-#    db.commit()
+def insert(review_id, review_name, review_body, positive, negative, neutral, polarity):
+    query = "INSERT OR UPDATE INTO " + table_name + " VALUES (" + query_plain + ")"
+    cursor.execute(query)
+    db.commit()
 
 
 def do_sentiment_analysis(review_id, review_name, review_body):
@@ -67,7 +72,7 @@ with open(collection, 'rb') as csvfile:
         doc_array = doc.split(',')
         reviewId = doc_array[0]
         reviewName = doc_array[3]
-        reviewBody = doc_array[4]
+        reviewBody = doc_array[4].replace('"', '')
         do_sentiment_analysis(reviewId, reviewName, reviewBody)
 
 db.close()

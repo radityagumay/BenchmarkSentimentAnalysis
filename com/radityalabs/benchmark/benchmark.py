0bd8f751e4a90c70e4d29246d1fe36bcbb1b9149
radityagumay <gumay.raditya@yahoo.com>
2017-03-10 09:12:55 +0700

    refactor

--- com/radityalabs/benchmark/benchmark.py
+++ com/radityalabs/benchmark/benchmark.py
@@ -1,26 +1,17 @@
 from HTMLParser import HTMLParser
-import scrapy
 import requests
 
 url = 'http://text-processing.com/demo/sentiment/'
 payload = {'language': 'english', 'text': "bad person"}
 r = requests.post(url, data=payload)
-print r.url
-print r.text
+s = r.text
+
+print "Url", r.url
+print "Html Response", s
 
 
 class Parser(HTMLParser):
-    #    def handle_starttag(self, tag, attrs):
-    # print "Encountered a start tag:", tag
-    # print "Start tag:", tag
-    # for attr in attrs:
-    #    print "     attr:", attr
-
-    # def handle_endtag(self, tag):
-    #    print "Encountered an end tag :", tag
-
     def handle_data(self, data):
-        #print "Encountered some data  :", data
         if "pos" in data:
             print data
         if "neg" in data:
@@ -32,4 +23,4 @@ class Parser(HTMLParser):
 
 
 parser = Parser()
-parser.feed(r.text)
+parser.feed(s)
